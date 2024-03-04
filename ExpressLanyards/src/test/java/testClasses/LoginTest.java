c45b7e7bedd68357404b881e5a0d8b925ea79f99
Jay Soni <89600182+jaysoni1806@users.noreply.github.com>
2024-03-04 15:59:42 +0530

    New Pull Commite

--- ExpressLanyards/src/test/java/testClasses/LoginTest.java
+++ ExpressLanyards/src/test/java/testClasses/LoginTest.java
@@ -12,5 +12,14 @@ public class LoginTest extends BeforeAfterTest{
 	@Test
 	public void loginTestMethod7() {
 		
+		
+	}
+	@Test
+	public void loginTestMethod9() {
+		
+	}
+	@Test
+	public void loginTestMethod10() {
+		
 	}
 }
