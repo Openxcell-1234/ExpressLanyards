2e2b6f478afc2138a9304f6e76df8592e25f3019
Jay Soni <89600182+jaysoni1806@users.noreply.github.com>
2024-03-04 16:01:35 +0530

    NEw Class Added by Jay

---
+++ ExpressLanyards/src/test/java/testClasses/SignUpClass.java
@@ -0,0 +1,24 @@
+package testClasses;
+
+import org.testng.annotations.Test;
+
+public class SignUpClass {
+	
+	@Test
+	public void loginTestMethod7() {
+		
+	}
+	@Test
+	public void loginTestMethod5() {
+		
+	}
+	@Test
+	public void loginTestMethod4() {
+		
+	}
+	@Test
+	public void loginTestMethod6() {
+		
+	}
+
+}
